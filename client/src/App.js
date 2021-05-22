d13064b59ba46ee4223bdedc5715e37ca4186559
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-05-22 15:05:51 +0530

    mid-section completed

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import GetATour from "./components/GetATour";
+
+function App() {
+  return <div><GetATour></GetATour></div>;
+}
+
+export default App;
---
+++ client/src/components/GetATour.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./GetATour.css";
+function GetATour() {
+  return (
+    <div className="outer">
+      <div className="getATour">
+        <div className="colorLine"></div>
+        <p className="gettext">get a tour</p>
+        <div className="getcontent">
+          <p className="intro">Checkout how we transform outdoor spaces</p>
+          <p className="intro">for our customers</p>
+        </div>
+      </div>
+      <div className="border">
+        <main className="page-content">
+          <div className="card">
+            <div className="content">
+              <h2 className="title">Front Lawn</h2>
+              <p>New Delhi</p>
+              <button className="btn1">View</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="content">
+              <h2 className="title">Terrace Garden</h2>
+              <p>New Delhi</p>
+              <button className="btn1">View</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="content">
+              <h2 className="title">Backyard</h2>
+              <p>Panipat</p>
+              <button className="btn1">View</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="content">
+              <h2 className="title">Terrace Garden</h2>
+              <p>New Delhi</p>
+              <button className="btn1">View</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="content">
+              <h2 className="title">Backyard</h2>
+              <p>Gurgaon</p>
+              <button className="btn1">View</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="content">
+              <h2 className="title">Terrace Garden</h2>
+              <p>New Delhi</p>
+              <button className="btn1">View</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="content">
+              <h2 className="title">Balcony Garden</h2>
+              <p>Gurgaon</p>
+              <button className="btn1">View</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="content">
+              <h2 className="title">Lawn</h2>
+              <p>Noida</p>
+              <button className="btn1">View</button>
+            </div>
+          </div>
+          <button className="btn btn-success">See all Projects and Designs</button>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default GetATour;
