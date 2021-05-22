a7c1124676b385a845c65491d6aae85de5716adf
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-05-22 21:46:52 +0530

    3 sections done

---
+++ client/src/components/top.js
@@ -0,0 +1,78 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import "./top.css";
+function Services() {
+  return (
+    <div className="container1">
+      <div className="getATour">
+        <div className="colorLine"></div>
+        <p className="gettext">Outdoor living for next generation</p>
+        <div className="getcontent">
+          <p className="intro">
+            Transform your outdoor spaces such as terraces, lawns and backyards
+            to enjoy with your friends and family and to host get togethers at
+            your home. Ecoventure transforms your outdoor spaces by designing
+            and transforming them with the help of professional designers and
+            execution team of engineers and installers
+          </p>
+        </div>
+        <div>
+          <div className="first-row">
+            <div className="element">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              <p>Terrace Garden</p>
+            </div>
+            <div className="element">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              <p>Terrace Garden</p>
+            </div>
+            <div className="element">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              <p>Terrace Garden</p>
+            </div>
+            <div className="element">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              <p>Terrace Garden</p>
+            </div>
+            <div className="element">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              <p>Terrace Garden</p>
+            </div>
+            <div className="element">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              <p>Terrace Garden</p>
+            </div>
+          </div>
+          <div className="second-row">
+            <div className="element">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              <p>Terrace Garden</p>
+            </div>
+            <div className="element">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              <p>Terrace Garden</p>
+            </div>
+            <div className="element">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              <p>Terrace Garden</p>
+            </div>
+            <div className="element">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              <p>Terrace Garden</p>
+            </div>
+            <div className="element">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              <p>Terrace Garden</p>
+            </div>
+            <div className="element">
+              <i class="fa fa-home" aria-hidden="true"></i>
+              <p>Terrace Garden</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
