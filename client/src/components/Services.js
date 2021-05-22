97b6e3bb00105dcc84b0670679dfd911119cce3f
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-05-22 17:44:34 +0530

    services

---
+++ client/src/components/Services.js
@@ -0,0 +1,164 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import "./Services.css";
+function Services() {
+  return (
+    <div className="container1">
+      <section class="cards">
+        <article class="card card--1">
+          <div class="card__img"></div>
+          <a href="#" class="card_link">
+            <div class="card__img--hover"></div>
+          </a>
+          <div class="card__info">
+            <h3 class="card__title">Swatter</h3>
+            <span class="card__by">
+              <a href="#" class="card__author" title="author">
+                <i class="fa fa-user" aria-hidden="true"></i> 123 professionals
+              </a>
+              <p className="stars">
+                <i class="fa fa-star" aria-hidden="true"></i> 4.9 (12345
+                approved reviews)
+              </p>
+            </span>
+          </div>
+        </article>
+        <article class="card card--2">
+          <div class="card__img"></div>
+          <a href="#" class="card_link">
+            <div class="card__img--hover"></div>
+          </a>
+          <div class="card__info">
+            <h3 class="card__title">Pest Control</h3>
+            <span class="card__by">
+              <a href="#" class="card__author" title="author">
+                <i class="fa fa-user" aria-hidden="true"></i> 123 professionals
+              </a>
+              <p className="stars">
+                <i class="fa fa-star" aria-hidden="true"></i> 4.9 (12345
+                approved reviews)
+              </p>
+            </span>
+          </div>
+        </article>
+        <article class="card card--3">
+          <div class="card__img"></div>
+          <a href="#" class="card_link">
+            <div class="card__img--hover"></div>
+          </a>
+          <div class="card__info">
+            <h3 class="card__title">Terrace Closure</h3>
+            <span class="card__by">
+              <a href="#" class="card__author" title="author">
+                <i class="fa fa-user" aria-hidden="true"></i> 123 professionals
+              </a>
+              <p className="stars">
+                <i class="fa fa-star" aria-hidden="true"></i> 4.9 (12345
+                approved reviews)
+              </p>
+            </span>
+          </div>
+        </article>
+
+        <article class="card card--4">
+          <div class="card__info-hover"></div>
+          <div class="card__img"></div>
+          <a href="#" class="card_link">
+            <div class="card__img--hover"></div>
+          </a>
+          <div class="card__info">
+            <span class="card__category"></span>
+            <h3 class="card__title"> Pool Construction</h3>
+            <span class="card__by">
+              <a href="#" class="card__author" title="author">
+                <i class="fa fa-user" aria-hidden="true"></i> 123 professionals
+              </a>
+              <p className="stars">
+                <i class="fa fa-star" aria-hidden="true"></i> 4.9 (12345
+                approved reviews)
+              </p>
+            </span>
+          </div>
+        </article>
+      </section>
+      <section className="cards">
+        <article class="card card--5">
+          <div class="card__img"></div>
+          <a href="#" class="card_link">
+            <div class="card__img--hover"></div>
+          </a>
+          <div class="card__info">
+            <h3 class="card__title">Glass Balcony</h3>
+            <span class="card__by">
+              <a href="#" class="card__author" title="author">
+                <i class="fa fa-user" aria-hidden="true"></i> 123 professionals
+              </a>
+              <p className="stars">
+                <i class="fa fa-star" aria-hidden="true"></i> 4.9 (12345
+                approved reviews)
+              </p>
+            </span>
+          </div>
+        </article>
+        <article class="card card--6">
+          <div class="card__img"></div>
+          <a href="#" class="card_link">
+            <div class="card__img--hover"></div>
+          </a>
+          <div class="card__info">
+            <h3 class="card__title">Ready roll Grass</h3>
+            <span class="card__by">
+              <a href="#" class="card__author" title="author">
+                <i class="fa fa-user" aria-hidden="true"></i> 123 professionals
+              </a>
+              <p className="stars">
+                <i class="fa fa-star" aria-hidden="true"></i> 4.9 (12345
+                approved reviews)
+              </p>
+            </span>
+          </div>
+        </article>
+        <article class="card card--7">
+          <div class="card__img"></div>
+          <a href="#" class="card_link">
+            <div class="card__img--hover"></div>
+          </a>
+          <div class="card__info">
+            <h3 class="card__title">A.C. Installation</h3>
+            <span class="card__by">
+              <a href="#" class="card__author" title="author">
+                <i class="fa fa-user" aria-hidden="true"></i> 123 professionals
+              </a>
+              <p className="stars">
+                <i class="fa fa-star" aria-hidden="true"></i> 4.9 (12345
+                approved reviews)
+              </p>
+            </span>
+          </div>
+        </article>
+
+        <article class="card card--8">
+          <div class="card__info-hover"></div>
+          <div class="card__img"></div>
+          <a href="#" class="card_link">
+            <div class="card__img--hover"></div>
+          </a>
+          <div class="card__info">
+            <h3 class="card__title">Paint Whitewash </h3>
+            <span class="card__by">
+              <a href="#" class="card__author" title="author">
+                <i class="fa fa-user" aria-hidden="true"></i> 123 professionals
+              </a>
+              <p className="stars">
+                <i class="fa fa-star" aria-hidden="true"></i> 4.9 (12345
+                approved reviews)
+              </p>
+            </span>
+          </div>
+        </article>
+      </section>
+    </div>
+  );
+}
+
+export default Services;
