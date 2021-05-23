a475a5f3f116da00ff2b05e97e408619e1c27a05
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-05-23 14:52:55 +0530

    contact section done

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 // import Services from "./components/Services";
 // import GetATour from "./components/GetATour";
 // import Top from "./components/top";
-import Blogs from "./components/Blogs";
+import GotQuestions from "./components/GotQuestions";
 function App() {
   return (
     <div>
-      <Blogs></Blogs>
+      <GotQuestions></GotQuestions>
     </div>
   );
 }
