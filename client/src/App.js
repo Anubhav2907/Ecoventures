b06b35eeb2fe37265dbb711b11c7840b20f538b4
Anubhav Singh <u19ec135@eced.svnit.ac.in>
2021-05-25 13:20:59 +0530

    Components Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
-
-import Home from "./components/Home"
+import Services from "./components/Services";
+import GetATour from "./components/GetATour";
+import Top from "./components/top";
+import Blogs from "./components/Blogs";
+import Instagram from "./components/instagram";
+import GotQuestions from "./components/GotQuestions";
+import Choose from "./components/Choose";
+import Navbar from "./components/NavBar";
+import NavBar2 from "./components/NavBar2";
+import Review from "./components/Reviews";
+import Footer from "./components/Footer";
 import Landscaping from "./components/Landscaping";
 import GreenVilla from "./components/GreenVilla";
-
-
+import GreenRoof from "./components/GreenRoof";
+import AirPurifier from "./components/AirPurifier";
+import VerticalGarden from "./components/VerticalGarden";
+import KitchenGarden from "./components/KitchenGarden";
 function App() {
   return (
     <div>
-      <Home />
-      {/* <Landscaping></Landscaping>
-      <GreenVilla></GreenVilla> */}
+      <KitchenGarden></KitchenGarden>
     </div>
   );
 }
