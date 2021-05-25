275c51935537a0342ae1044c94b6240aba03e787
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-05-26 03:43:53 +0530

    issues resolved

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import Home from "./components/Home";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Container } from "@material-ui/core";
@@ -30,30 +29,6 @@ function App() {
         </Switch>
       </Container>
     </BrowserRouter>
-=======
-import Services from "./components/Services";
-import GetATour from "./components/GetATour";
-import Top from "./components/top";
-import Blogs from "./components/Blogs";
-import Instagram from "./components/instagram";
-import GotQuestions from "./components/GotQuestions";
-import Choose from "./components/Choose";
-import Navbar from "./components/NavBar";
-import NavBar2 from "./components/NavBar2";
-import Review from "./components/Reviews";
-import Footer from "./components/Footer";
-import Landscaping from "./components/Landscaping";
-import GreenVilla from "./components/GreenVilla";
-import GreenRoof from "./components/GreenRoof";
-import AirPurifier from "./components/AirPurifier";
-import VerticalGarden from "./components/VerticalGarden";
-import KitchenGarden from "./components/KitchenGarden";
-function App() {
-  return (
-    <div>
-      <KitchenGarden></KitchenGarden>
-    </div>
->>>>>>> master
   );
 }
 
--- client/src/components/NavBar2.js
+++ client/src/components/NavBar2.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { AppBar, Toolbar, Button, Menu, MenuItem } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
@@ -17,7 +16,9 @@ const NavBar2 = () => {
   return (
     <AppBar position="static" color="transparent">
       <Toolbar>
-        <Button component={Link} to="/TerraceGarden">Terrace gardans</Button>
+        <Button component={Link} to="/TerraceGarden">
+          Terrace gardans
+        </Button>
         <Button
           aria-controls="simple-menu"
           aria-haspopup="true"
@@ -51,49 +52,11 @@ const NavBar2 = () => {
             Kitchen Garden
           </MenuItem>
         </Menu>
-=======
-import { AppBar, Toolbar, Typography, Button ,Menu,MenuItem} from "@material-ui/core";
-
-const NavBar2 = () => 
-{
-    const [anchorEl, setAnchorEl] = React.useState(null);
-  
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-  
-  return (
-    <AppBar position="static" color="transparent">
-      <Toolbar>
-        <Button>Terrace gardans</Button>
-        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-        Lawns & Backyard
-      </Button>
-      <Menu
-        id="menu-list-grow"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>Landscaping</MenuItem>
-        <MenuItem onClick={handleClose}>Green Villa</MenuItem>
-        <MenuItem onClick={handleClose}>Green Roof</MenuItem>
-        <MenuItem onClick={handleClose}>Air Purifier</MenuItem>
-        <MenuItem onClick={handleClose}>Vertical Garden</MenuItem>
-        <MenuItem onClick={handleClose}>Plant Scaping</MenuItem>
-        <MenuItem onClick={handleClose}>Kitchen Garden</MenuItem>
-      </Menu>
->>>>>>> master
         <Button>How it works?</Button>
         <Button style={{ fontWeight: "bold" }}>book mali</Button>
       </Toolbar>
     </AppBar>
   );
-}
+};
 
 export default NavBar2;
